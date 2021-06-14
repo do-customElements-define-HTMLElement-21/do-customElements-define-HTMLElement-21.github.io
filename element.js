6dc8a69d8ed7a84982d571401f8ec4b06043eba0
Danny Engelman <danny@engelman.nl>
2021-06-14 14:50:11 +0200

    secret 1

--- element.js
+++ element.js
@@ -1,8 +1,11 @@
+console.log(document.currentScript.src);
+
 let d = document.currentScript.src.split`-`;
 d = d.concat(atob(d[5]).split`,`);
-window[d[1]][d[2]](
-  d[6],
-  class extends window[d[3]] {
+
+console.log("%c Dictionary: \n","background:red",d.map((word,idx)=>console.log(idx,word)));
+
+window[d[1]][d[2]]( d[6], class extends window[d[3]] {
     connectedCallback() {
       this[d[7]] = d[6];
     }
